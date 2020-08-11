54df6f81e4a2e509c1082a6cc81cddc554d7597b
poe1993 <poe1993@yahoo.com>
2020-08-11 14:45:44 -0400

    I don't know what this is doing

--- Program.cs
+++ Program.cs
@@ -2,11 +2,38 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var numberOfCupsOfCoffee = 1;
+            var fullName = ("Eric Poe");
+            var today = ("August 11");
+            Console.WriteLine(numberOfCupsOfCoffee + " " + fullName + " " + today);
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"Greetings {userName}!");
+            Console.WriteLine("Can you give me two numbers?");
+            var firstNumberAsString = Console.ReadLine();
+            var secondNumberAsString = Console.ReadLine();
+            var firstOperand = double.Parse(firstNumberAsString);
+            var secondOperand = double.Parse(secondNumberAsString);
+            Console.WriteLine(firstOperand + " " + secondOperand);
+            var sum = (firstOperand + secondOperand);
+            Console.WriteLine($"Your sum is {sum}");
+            var difference = (firstOperand - secondOperand);
+            Console.WriteLine($"Your difference is {difference}");
+            var product = (firstOperand * secondOperand);
+            Console.WriteLine($"Your product is {product}");
+            var quotient = (firstOperand / secondOperand);
+            Console.WriteLine($"Your quotient is {quotient}");
+            var remainder = (firstOperand % secondOperand);
+            Console.WriteLine($"Your remainder is {remainder}");
+
+
+
+
+
+        }
     }
-  }
 }
